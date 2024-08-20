66dad8d9f2f9be19e35d345cccb9b48038e0d49d
Eng Sameh <eng_sameh_86@outlook.com>
2024-08-20 23:48:30 +0400

    new PROJECT evenoddnumbersapp

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        PrintNumbers("All Numbers", ints);
+        PrintNumbers("Odd Numbers", ints.Where(x=>IsOdd(x)));
+        PrintNumbers("Even Numbers", ints.Where(x=>IsEven(x)));
+    }
+    static void PrintNumbers(string Title,IEnumerable<int> nums) {
+        Console.Write($"{Title} : ["); 
+        foreach (int i in nums)
+        {
+            Console.Write(i+",");
+        }
+        Console.WriteLine("]");
+    }
+    static bool IsOdd(int n)
+    {
+        if (n % 2 == 0) {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    static bool IsEven(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
